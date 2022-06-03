f145c5e9b79c515335d366bb68368c6401acdffd
Sabrina Sun <sunjs4@student.unimelb.edu.au>
2022-06-03 22:24:08 +1000

    js of contact and confirmation page

--- js/main.js
+++ js/main.js
@@ -95,16 +95,26 @@ window.onscroll = () =>{
     document.addEventListener("click", closeAllSelect);
 
 
-//contact seller & enquiry
-// document.getElementById("contactseller").addEventListener("click", function(){
-//     document.querySelector(".contact-form").style.display = "flex";
-// })
+// contact seller & enquiry
 
-// document.querySelector(".fa-xmark").addEventListener("click", function(){
-//     document.querySelector(".contact-form").style.display = "none";
-// })
 
-// document.getElementsById("submit-btn").addEventListener("click", function(){
-//     document.querySelector(".contact-form").style.display = "none",
-//     document.querySelector(".contact-confirm").style.display = "flex";
-// })
+
+document.getElementById("contactseller").addEventListener("click", function(){
+    document.querySelector(".contact-form").style.display = "flex";
+})
+
+document.querySelector(".fa-xmark").addEventListener("click", function(){
+    document.querySelector(".contact-form").style.display = "none";
+})
+
+
+// confirm & close confirm
+
+document.getElementById("submit-btn").addEventListener("click", function(){
+    document.querySelector(".contact-confirm").style.display = "flex";
+    document.querySelector(".contact-form").style.display = "none";
+})
+
+document.getElementById("confirm-xmark").addEventListener("click", function(){
+    document.querySelector(".contact-confirm").style.display = "none";
+})
